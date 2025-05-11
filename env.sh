cb0c5b3ead7c69aa932136182f6e03b9b27219c6
iuliet716 <62701789+iuliet716@users.noreply.github.com>
2025-05-11 21:13:38 +0900

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# WARNING: You MUST run this script using 'source env.sh' (not './env.sh')
+
+# Directory Path
+ENV_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Setting
+# Python Release: https://www.python.org/downloads/
+ENV_NAME="cu128"
+PYTHON_VERSION="3.12.10"
+
+# Environment Variable
+## PYENV
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+## CUDA
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+
+echo "Install Python $PYTHON_VERSION"
+pyenv install -s $PYTHON_VERSION
+
+echo "Create Python Virtual Env: $ENV_NAME"
+pyenv virtualenv $PYTHON_VERSION $ENV_NAME
+
+echo "Activate Python Virtual Env"
+pyenv activate $ENV_NAME
+
+echo "Upgrade pip and install requirements"
+pip install --upgrade pip
+pip install -r $ENV_DIRECTORY/torch_requirements.txt
+pip install -r $ENV_DIRECTORY/requirements.txt
